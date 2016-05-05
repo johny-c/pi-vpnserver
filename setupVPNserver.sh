ee0790240d917d0ac5ce1730df4c7f5b25a2f3cb
John Chiotellis <ioannis.chiotellis@gmail.com>
2016-05-06 01:19:26 +0200

    final small fixes

--- setupVPNserver.sh
+++ setupVPNserver.sh
@@ -31,15 +31,16 @@ sudo chown -R $USER:$USER $ERDIR
 
 ## Edit the vars file - Use '@' as sed delimiter because we use / already
 printf '\nEditing the "vars" file - export EASY_RSA="/etc/openvpn/easy-rsa"...\n'
-fnew=$ERDIR/vars
+fpath=$ERDIR/vars
 forig="$ORIGERDIR/vars"
-lineold=$(cat $forig | grep "export EASY_RSA")
-linenew="export EASY_RSA=$ERDIR"
+old=$(cat $forig | grep "export EASY_RSA")
+new=$(printf 'export EASY_RSA="%s"' "$ERDIR")
+printf "\nNow replacing %s \nwith %s \nin %s\n" "$old" "$new" $fpath
 sed -i -- "s@$lineold@$linenew@g" $fnew
 
 ## Build certificate authority
 printf "\nNext is building a certificate authority!\n\n"
-read -p "Press any key to start building the CA\n\n"
+read -p "Press any key to start building the CA"
 . $CWD/utils.sh
 cd $ERDIR
 . ./vars
@@ -51,7 +52,7 @@ SERVER_NAME=$(read_from_yaml $CFG_FILE "SERVER_NAME")
 printf "\nNow building the key server\n\n
         Common name must be the same as the server name (%s)\n
         Leave the challenge password blank.\n\n" $SERVER_NAME
-read -p "Press any key to start building the Key Server\n\n"
+read -p "Press any key to start building the Key Server"
 ./build-key-server $SERVER_NAME
 
 # ## Build the client keys for your server, enter a vpn username
@@ -115,7 +116,7 @@ fi
 printf "\nUpdating %s \nwith %s\n\n" "$ETCDIR/network/interfaces" "$ETCDIR/$fwrules"
 IFACE_TYPE=$(read_from_yaml $CFG_FILE "IFACE_TYPE")
 old=$(cat "$ETCDIR/network/interfaces" | grep "iface $IFACE_TYPE inet ")
-new="$old\tpre-up $ETCDIR/$fwrules"
+new=$(printf "old\tpre-up %s/%s" "$ETCDIR" "$fwrules")
 printf "\nNow replacing %s \nwith %s \nin %s\n" "$old" "$new" $fpath
 sudo sed -i -- "s@$old@$new@g" "$ETCDIR/network/interfaces"
 
@@ -154,11 +155,11 @@ CLIENT_NAMES=( $(pcregrep -M '^  .*\n' $CFG_FILE | cut -d : -f 1 ) )
 ## Build the client keys for your server, enter a vpn username
 printf "Now building the client keys. Leave the challenge passwords blank.\n\n"
 for client in ${CLIENT_NAMES[@]}; do
-    print "Building key for client: %s\n\n" $client
-    . /vars
+    printf "Building key for client: %s\n\n" "$client"
+    . ./vars
     ./build-key-pass $client
     openssl rsa -in "$client.key" -des3 -out "$client.3des.key"
-    ./keys/makeOVPN $client
+    ./keys/makeOVPN.sh $client
 done
 
 printf "\nClients should be ready! You just have to reboot\n"
